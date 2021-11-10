b3a56bc68f7a5d94a3f2484ba25428cb52e3da87
muenalakevin <kmmuenala@espe.edu.ec>
2021-11-09 20:50:55 -0500

    Untrack files in .gitignore

---
+++ app/middleware/origin.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const checkOrigin= (req, res, next)=>{
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !== 'undefined'){
+        const bearertoken = bearerHeader.split(" ")[1];
+        req.token = bearertoken;
+        jwt.verify(req.token,  process.env.SECRET_KEY ,(err, authData)=>{
+            if(err){
+                res.sendStatus(403);
+            }else{
+                next();
+            }
+        })
+        
+    }else{
+        res.sendStatus(403);
+    }
+}
+
+module.exports = checkOrigin
