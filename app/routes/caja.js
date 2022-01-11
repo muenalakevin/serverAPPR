c9bae80a69e899f96c4bbb14a665bef6221dcf0f
muenalaDev <kmmuenala>
2022-01-11 18:07:07 -0500

    Cuarto sprint

--- app/routes/caja.js
+++ app/routes/caja.js
@@ -8,6 +8,7 @@ const {  getCaja,createItem} = require('../controllers/caja')
 
 router.get('/',checkOrigin,JWT, getCaja)
 router.post('/',checkOrigin,JWT, createItem)
+router.patch('/',checkOrigin,JWT, createItem)
 
 
 
